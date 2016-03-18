1e7b0d8bc81b166f833e9b7afb2ff0025c058b3b
jonas lundberg <josnase@gmail.com>
2016-03-18 17:19:43 +0100

    Separate file for sensitive information such as database password

---
+++ systemetApi.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var exp = {
+    objects: {},
+    settings: {},
+    database: {
+        host: "localhost",
+        port: 3306,
+        name: "systemet",
+        user: "root",
+        password: "systemet"
+    }
+};
+
+module.exports = exp;

c3285f6396956294c236b3cbb3002eb94a1c20cc
jonas lundberg <josnase@gmail.com>
2016-03-21 17:36:23 +0100

    changed APK to apk

--- mysqlScripts/products.sql
+++ mysqlScripts/products.sql
@@ -37,8 +37,8 @@ SET Alkoholhalt = SUBSTRING(Alkoholhalt, 1, CHAR_LENGTH(Alkoholhalt) - 1)
 WHERE Alkoholhalt LIKE '%%';
 ALTER TABLE products MODIFY Alkoholhalt DECIMAL(4,2);
 
-ALTER TABLE products ADD APK DECIMAL(5,2);
-UPDATE products SET APK = ((Alkoholhalt/100)*Volymiml)/Prisinklmoms;
+ALTER TABLE products ADD apk DECIMAL(5,2);
+UPDATE products SET apk = ((Alkoholhalt/100)*Volymiml)/Prisinklmoms;
 
 
 

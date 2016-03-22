4a91f7b722c53e81a48a691e1e6f6194a3d41414
jonas <t4.jonas.lundberg@umea.nti.se>
2016-03-22 13:49:01 +0100

    save password for mysql in variable and use it to run sql commands

--- mysqlScripts/getProducts.sh
+++ mysqlScripts/getProducts.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
-sudo mysql -u root -p < products.sql
-curl https://www.systembolaget.se/api/assortment/products/xml > /tmp/bolaget.xml ; mysql -u root -p -e "LOAD XML LOCAL INFILE '/tmp/bolaget.xml' INTO TABLE products ROWS IDENTIFIED BY '<artikel>';" systemet
-mysql -u root -p systemet < update.sql
+echo "Password to use for mysql:"
+read password
+sudo mysql -u root -p$password < products.sql
+curl https://www.systembolaget.se/api/assortment/products/xml > /tmp/bolaget.xml ; mysql -u root -p$password -e "LOAD XML LOCAL INFILE '/tmp/bolaget.xml' INTO TABLE products ROWS IDENTIFIED BY '<artikel>';" systemet
+mysql -u root -p$password systemet < update.sql

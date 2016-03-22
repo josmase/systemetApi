5a67ecf65307c7171b390aae837ee320a04b283f
jonas <t4.jonas.lundberg@umea.nti.se>
2016-03-22 14:13:56 +0100

    renamed to better fit use

--- mysqlScripts/setupDatabase.sh
+++ mysqlScripts/setupDatabase.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-echo "Password to use for mysql:"
-read password
+read -s -p "Password to use for mysql:" password
 sudo mysql -u root -p$password < products.sql
 curl https://www.systembolaget.se/api/assortment/products/xml > /tmp/bolaget.xml ; mysql -u root -p$password -e "LOAD XML LOCAL INFILE '/tmp/bolaget.xml' INTO TABLE products ROWS IDENTIFIED BY '<artikel>';" systemet
 mysql -u root -p$password systemet < update.sql

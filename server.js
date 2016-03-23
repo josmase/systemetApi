130a883017d5fd50cec99cb7259d81777f2c954d
jonas lundberg <josnase@gmail.com>
2016-03-23 20:09:39 +0100

    changed port and removed som old comments

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-// server.js
 
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
-var express = require('express');        // call express
-var app = express();                 // define our app using express
+var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var cors = require('cors');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(cors());
 
-var port = process.env.PORT || 8080;        // set our port
+var port = process.env.PORT || 8000;
 
 var systemetapi = require('./systemetApi.js');
 
@@ -38,18 +33,15 @@ database.connect(function (err) {
     console.log('connected as id ' + database.threadId);
 });
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+var router = express.Router();
 
 // middleware to use for all requests
 router.use(function (req, res, next) {
     // do logging
     console.log(req.query);
-    next(); // make sure we go to the next routes and don't stop here
+    next();
 });
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function (req, res) {
     res.json({message: 'hooray! welcome to our api!'});
 });
@@ -67,7 +59,13 @@ router.get('/products', function (req, res) {
        }
    }
 
-    sql += " ORDER BY apk DESC LIMIT 10";
+    sql += " ORDER BY apk DESC LIMIT 500";
+    databaseQuery(sql, inserts, res);
+});
+router.get('/products/:id', function (req, res) {
+    console.log(req.params.id);
+    var sql = "SELECT * FROM products WHERE Artikelid = ?";
+    var inserts = [req.params.id];
     databaseQuery(sql, inserts, res);
 });
 
@@ -113,11 +111,8 @@ function databaseQuery(sql, inserts, res) {
 }
 
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
+
 app.use('/api', router);
 
-// START THE SERVER
-// =============================================================================
 app.listen(port);
 console.log('Magic happens on port ' + port);

b2ac0c1712107d51ca536dfbd38d315d6e1eba28
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-13 16:05:08 +0200

    use url

--- database.js
+++ database.js
@@ -94,46 +94,46 @@ function setup() {
 
 function insert() {
     console.log("Getting data to insert");
-    /* request.get('https://www.systembolaget.se/api/assortment/products/xml', function (error, response, data) {
-     if (!error && response.statusCode == 200) {
-     */
-
     return new Promise(function (resolve, reject) {
-        fs.readFile(__dirname + '/doodle.xml', function (err, data) {
-                if (err)reject(err);
+        request.get('https://www.systembolaget.se/api/assortment/products/xml', function (error, response, data) {
+                if (!error && response.statusCode == 200) {
 
-                var options = {
-                    object: true,
-                    reversible: false,
-                    coerce: true,
-                    sanitize: true,
-                    trim: true,
-                    arrayNotation: false
-                };
-                var result = parser.toJson(data, options);
-                var inserts = [];
+                    var options = {
+                        object: true,
+                        reversible: false,
+                        coerce: true,
+                        sanitize: true,
+                        trim: true,
+                        arrayNotation: false
+                    };
+                    var result = parser.toJson(data, options);
+                    var inserts = [];
 
-                var articles = result.artiklar.artikel;
+                    var articles = result.artiklar.artikel;
 
-                console.log(articles.length);
-                var columns = ['nr', 'Artikelid', 'Varnummer', 'Namn', 'Namn2', 'Prisinklmoms', 'Pant', 'Volymiml',
-                    'PrisPerLiter', 'Saljstart', 'Slutlev', 'Varugrupp', 'Forpackning', 'Forslutning', 'Ursprung',
-                    'Ursprunglandnamn', 'Producent', 'Leverantor', 'Argang', 'Provadargang', 'Alkoholhalt', 'Sortiment',
-                    'Ekologisk', 'Etiskt', 'Koscher', 'RavarorBeskrivning'];
+                    console.log(articles.length);
+                    var columns = ['nr', 'Artikelid', 'Varnummer', 'Namn', 'Namn2', 'Prisinklmoms', 'Pant', 'Volymiml',
+                        'PrisPerLiter', 'Saljstart', 'Slutlev', 'Varugrupp', 'Forpackning', 'Forslutning', 'Ursprung',
+                        'Ursprunglandnamn', 'Producent', 'Leverantor', 'Argang', 'Provadargang', 'Alkoholhalt', 'Sortiment',
+                        'Ekologisk', 'Etiskt', 'Koscher', 'RavarorBeskrivning'];
 
-                var sql = "INSERT INTO products (??)  VALUES ? ON DUPLICATE KEY UPDATE ??=values(??)";
-                console.log("Building query");
-                var i, j, temparray, chunk = 100;
-                for (i = 0, j = articles.length; i < j; i += chunk) {
-                    temparray = articles.slice(i, i + chunk);
-                    buildInsertQuery(temparray, columns, sql, inserts)
-                        .then(function (data) {
-                            databaseQuery(sql, [columns, data, columns[1], columns[1]])
-                                .then((result) => resolve(result))
-                                .catch((err) => reject(err));
-                        }).catch(function (err) {
-                        reject(err)
-                    });
+                    var sql = "INSERT INTO products (??)  VALUES ? ON DUPLICATE KEY UPDATE ??=values(??)";
+                    console.log("Building query");
+                    var i, j, temparray, chunk = 100;
+                    for (i = 0, j = articles.length; i < j; i += chunk) {
+                        temparray = articles.slice(i, i + chunk);
+                        buildInsertQuery(temparray, columns, sql, inserts)
+                            .then(function (data) {
+                                databaseQuery(sql, [columns, data, columns[1], columns[1]])
+                                    .then((result) => resolve(result))
+                                    .catch((err) => reject(err));
+                            }).catch(function (err) {
+                            reject(err)
+                        });
+                    }
+                }
+                else {
+                    reject(error)
                 }
             }
         );

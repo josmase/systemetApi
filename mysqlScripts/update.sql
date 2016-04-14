37ab8569b5941c95cd874465dc11819588376d1e
jonas <t4.jonas.lundberg@umea.nti.se>
2016-04-14 09:08:25 +0200

    mode creating of apk column to initial schema creation

--- mysqlScripts/update.sql
+++ mysqlScripts/update.sql
@@ -2,7 +2,8 @@
 UPDATE products
 SET Alkoholhalt = SUBSTRING(Alkoholhalt, 1, CHAR_LENGTH(Alkoholhalt) - 1)
 WHERE Alkoholhalt LIKE '%%';
+
 ALTER TABLE products MODIFY Alkoholhalt DECIMAL(4,2);
 
-ALTER TABLE products ADD apk DECIMAL(5,2);
 UPDATE products SET apk = ((Alkoholhalt/100)*Volymiml)/Prisinklmoms;
+

6eb4b11f4aaf20bfba1d439aad15a95c6b6d0973
jonas <josnase@gmail.com>
2017-05-20 20:14:09 +0200

    Change route to app

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const cluster = require('cluster');
 const http = require('http');
 const numCPUs = require('os').cpus().length;
-var database = require('./database.js');
+const database = require('./database.js');
 
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
-database.setup();
-    
+  database.setup();
+
   // Fork workers.
-  for (var i = 0; i < numCPUs; i++) {
+  for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
@@ -16,142 +16,130 @@ database.setup();
     console.log(`worker ${worker.process.pid} died`);
   });
 } else {
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var helmet = require('helmet');
+  const express = require('express');
+  const app = express();
+  const bodyParser = require('body-parser');
+  const cors = require('cors');
+  const helmet = require('helmet');
 
-var geocoderProvider = 'google';
-var httpAdapter = 'https';
-var extra = {
+  const geocoderProvider = 'google';
+  const httpAdapter = 'https';
+  const extra = {
     apiKey: 'AIzaSyAz9VB62M7bhTVi5qmToMnrqdbQjq5Xugk',
-    formatter: 'json'
-};
+    formatter: 'json',
+  };
 
-var geocoder = require('node-geocoder')(geocoderProvider, httpAdapter, extra);
+  const geocoder = require('node-geocoder')(geocoderProvider, httpAdapter, extra);
 
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(cors());
+  app.use(helmet());
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.json());
-app.use(cors());
-app.use(helmet());
-
-var port = process.env.PORT || 3000;
-
-var router = express.Router();
-
+  const port = process.env.PORT || 8081;
 
 // middleware to use for all requests
-router.use(function (req, res, next) {
+  app.use((req, res, next) => {
     next();
-});
+  });
 
-router.get('/', function (req, res) {
-    res.json({message: "Use /products"});
-});
+  router.get('/', (req, res) => {
+    res.json({ message: 'Use /products' });
+  });
 
-/**
- * Returns all the products matching the filter
- */
-router.get('/products', function (req, res) {
-    var sql = "SELECT * FROM products WHERE nr > 0";
-    var inserts = [];
-    var query = "";
-    for (var key in req.query) {
-        if (req.query.hasOwnProperty(key)) {
-            query = addToQueryIfExists(key, req.query);
-            sql += query.sql;
-            inserts.push(query.identifier);
-            inserts.push(query.value);
-        }
+  /**
+   * Returns all the products matching the filter
+   */
+  app.get('/products', (req, res) => {
+    let sql = 'SELECT * FROM products WHERE nr > 0';
+    const inserts = [];
+    let query = '';
+    for (const key in req.query) {
+      if (req.query.hasOwnProperty(key)) {
+        query = addToQueryIfExists(key, req.query);
+        sql += query.sql;
+        inserts.push(query.identifier);
+        inserts.push(query.value);
+      }
     }
 
-    sql += " ORDER BY apk DESC LIMIT 500";
+    sql += ' ORDER BY apk DESC LIMIT 500';
     database.query(sql, inserts)
-        .then((result) => res.json(result))
-        .catch((err) => res.json(err));
-});
+      .then(result => res.json(result))
+      .catch(err => res.json(err));
+  });
 
-/**
- * returns a product with the matching id
- */
-router.get('/product/:id', function (req, res) {
-    var sql = "SELECT * FROM products WHERE Artikelid = ?";
-    var inserts = [req.params.id];
+  /**
+   * returns a product with the matching id
+   */
+  app.get('/product/:id', (req, res) => {
+    const sql = 'SELECT * FROM products WHERE Artikelid = ?';
+    const inserts = [req.params.id];
     database.query(sql, inserts)
-        .then((result) => res.json(result))
-        .catch((err) => res.json(err));
-});
+      .then(result => res.json(result))
+      .catch(err => res.json(err));
+  });
 
-/**
- * Returns all stores in the same town as in the request. Works with latitude and longitude or the name of the city
- */
-router.get('/stores', function (req, res) {
-    var position = {
-        lat: Number(req.query.lat),
-        lon: Number(req.query.lon)
+  /**
+   * Returns all stores in the same town as in the request. Works with latitude and longitude or the name of the city
+   */
+  app.get('/stores', (req, res) => {
+    const position = {
+      lat: Number(req.query.lat),
+      lon: Number(req.query.lon),
     };
     if (position.lat) {
-        geocoder.reverse(position)
-            .then(function (response) {
-                var sql = "select * from stores where Address4 like ?";
-                var insert = [response[0].city];
-                database.query(sql, insert)
-                    .then((result)=>res.json(result))
-                    .catch((error)=>res.json(error))
-            })
-            .catch(function (err) {
-                res.json({
-                    success: false,
-                    message: 'Failed to get a position', err
-                });
-            });
-
+      geocoder.reverse(position)
+        .then((response) => {
+          const sql = 'select * from stores where Address4 like ?';
+          const insert = [response[0].city];
+          database.query(sql, insert)
+            .then(result => res.json(result))
+            .catch(error => res.json(error));
+        })
+        .catch((err) => {
+          res.json({
+            success: false,
+            message: 'Failed to get a position',
+            err,
+          });
+        });
+    } else {
+      const sql = 'SELECT * FROM stores WHERE Address4 like ?';
+      const inserts = [req.query.stad];
+      database.query(sql, inserts)
+        .then(result => res.json(result))
+        .catch(err => res.json(err));
     }
-    else {
+  });
 
-        var sql = "SELECT * FROM stores WHERE Address4 like ?";
-        var inserts = [req.query.stad];
-        database.query(sql, inserts)
-            .then((result) => res.json(result))
-            .catch((err) => res.json(err));
+  /**
+   * If last three chars is min or max return matching sql string, otherwise return a string matching by like
+   * @param key
+   * @param query
+   * @returns {*}
+   */
+  function addToQueryIfExists(key, query) {
+    if (key.slice(-3) === 'Max' && query[key] >= 0) {
+      return {
+        sql: ' AND ?? < ?',
+        value: query[key],
+        identifier: key.slice(0, -3),
+      };
+    } else if (key.slice(-3) === 'Min' && query[key] >= 0) {
+      return {
+        sql: ' AND ?? > ?',
+        value: query[key],
+        identifier: key.slice(0, -3),
+      };
     }
-});
+    return {
+      sql: ' AND ?? LIKE ?',
+      value: `%${query[key]}%`,
+      identifier: key,
+    };
+  }
 
-/**
- * If last three chars is min or max return matching sql string, otherwise return a string matching by like
- * @param key
- * @param query
- * @returns {*}
- */
-function addToQueryIfExists(key, query) {
-
-    if (key.slice(-3) === "Max" && query[key] >= 0) {
-        return {
-            sql: " AND ?? < ?",
-            value: query[key],
-            identifier: key.slice(0, -3)
-        }
-    }
-    else if (key.slice(-3) === "Min" && query[key] >= 0) {
-        return {
-            sql: " AND ?? > ?",
-            value: query[key],
-            identifier: key.slice(0, -3)
-        }
-    }
-    else {
-        return {
-            sql: " AND ?? LIKE ?",
-            value: "%" + query[key] + "%",
-            identifier: key
-        }
-    }
-}
-
-app.use('/api', router);
-
-    app.listen(port);
-     console.log(`Worker ${process.pid} started`);
+  app.listen(port);
+  console.log(`Worker ${process.pid} started`);
 }
